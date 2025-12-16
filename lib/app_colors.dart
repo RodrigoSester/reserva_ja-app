8cbff71b006fa7f6a6e8a72769c04648ebde2649
wigo <rodrigows09@gmail.com>
2025-12-16 18:44:33 -0300

    feat: add create custom app colors

---
+++ lib/app_colors.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF673AB7);
+  static const Color primary600 = Color(0xFF9575CD);
+  static const Color secondary = Color(0xFFFFC107);
+
+  static const Color primaryText = Color(0xFF363636);
+  static const Color secondaryText = Color(0xFF595757);
+
+  static const Color background = Color(0xFFF5F5F5);
+}
