071c9e0eaa7fa7889582c0692249c1244b9a4460
wigo <rodrigows09@gmail.com>
2025-12-19 00:26:25 -0300

    feat: add form validation and set of fields values

--- lib/screens/register_establishment_page.dart
+++ lib/screens/register_establishment_page.dart
@@ -1,19 +1,34 @@
-import 'package:flutter/material.dart';
 import 'package:reserva_ja/app_colors.dart';
 import 'package:reserva_ja/l10n/app_localizations.dart';
 import 'package:reserva_ja/widgets/text_input.dart';
+import 'package:flutter/material.dart';
 
-class RegisterEstablishmentPage extends StatelessWidget {
-  bool hasSnackbar;
-  bool hasLockerRoom;
-  List offeredSports;
+class RegisterEstablishmentPage extends StatefulWidget {
+  const RegisterEstablishmentPage({super.key});
 
-  RegisterEstablishmentPage({
-    super.key,
-    this.hasSnackbar = false,
-    this.hasLockerRoom = false,
-    this.offeredSports = const [],
-  });
+  @override
+  State<RegisterEstablishmentPage> createState() => _RegisterEstablishmentState();
+}
+
+class _RegisterEstablishmentState extends State<RegisterEstablishmentPage> {
+  String title = '';
+  String address = '';
+  double price = 0.0;
+  String description = '';
+  bool hasSnackbar = false;
+  bool hasLockerRoom = false;
+  List offeredSports = [];
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext ctx) {
+    if (_formKey.currentState != null) {
+      final isFormValid = _formKey.currentState!.validate();
+      if (isFormValid) {
+        _formKey.currentState!.save();
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext ctx) {
@@ -29,144 +44,187 @@ class RegisterEstablishmentPage extends StatelessWidget {
           },
         ),
       ),
-      body: Container(
-        padding: EdgeInsetsGeometry.symmetric(horizontal: 24.0, vertical: 32.0),
-        child: Column(
-          children: [
-            Text(
-              'Cadastre seu espaço',
-              style: TextTheme.of(ctx).titleLarge?.copyWith(
-                  fontSize: 32,
-                  color: AppColors.primaryText,
-                  fontWeight: FontWeight.w800
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 24.0, vertical: 32.0),
+          child: Column(
+            children: [
+              Text(
+                'Cadastre seu espaço',
+                style: TextTheme.of(ctx).titleLarge?.copyWith(
+                    fontSize: 32,
+                    color: AppColors.primaryText,
+                    fontWeight: FontWeight.w800
+                ),
               ),
-            ),
-            Text(
-              'Seu centro esportivo na palma mão',
-              style: TextTheme.of(ctx).bodyMedium?.copyWith(
-                color: AppColors.secondaryText,
-                fontWeight: FontWeight.w600,
-                fontSize: 16,
+              Text(
+                'Seu centro esportivo na palma mão',
+                style: TextTheme.of(ctx).bodyMedium?.copyWith(
+                  color: AppColors.secondaryText,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Form(
-              child: Column(
-                children: [
-                  TextInput(
-                    label: 'Nome do estabelecimento',
-                    required: true,
-                    decoration: InputDecoration(
-                        hintText: 'Nome do estabelecimento',
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(8))
-                        )
+              const SizedBox(height: 16.0),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextInput(
+                      label: 'Nome do estabelecimento',
+                      required: true,
+                      decoration: InputDecoration(
+                          hintText: 'Nome do estabelecimento',
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(8))
+                          )
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Campo obrigatório";
+                        }
+
+                        value.length < 5
+                          ? "Nome precisa ser maior do que 5"
+                          : null;
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        title = value!;
+                      },
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextInput(
-                    label: 'Endereço completo',
-                    required: true,
-                    decoration: InputDecoration(
+                    const SizedBox(height: 16.0),
+                    TextInput(
+                      label: 'Endereço completo',
+                      required: true,
+                      decoration: InputDecoration(
                         hintText: 'Park avenue, 1001',
                         fillColor: Colors.white,
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(8))
-                        )
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextInput(
-                    label: 'Custo por hora',
-                    required: true,
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    decoration: InputDecoration(
-                      prefixText: 'R\$',
-                      prefixStyle: TextStyle(
-                        color: AppColors.primaryText,
-                        fontWeight: FontWeight.w600
+                        ),
                       ),
-                      hintText: '100,00',
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8))
-                      )
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Campo obrigatório";
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        address = value!.trim();
+                      },
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextInput(
-                    label: 'Descrição (opcional)',
-                    decoration: InputDecoration(
+                    const SizedBox(height: 16.0),
+                    TextInput(
+                      label: 'Custo por hora',
+                      required: true,
+                      keyboardType: TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        prefixText: 'R\$',
+                        prefixStyle: TextStyle(
+                          color: AppColors.primaryText,
+                          fontWeight: FontWeight.w600
+                        ),
+                        hintText: '100,00',
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))
+                        )
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Campo obrigatório";
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        price = double.parse(value!);
+                        setState(() {});
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextInput(
+                      label: 'Descrição (opcional)',
+                      decoration: InputDecoration(
                         hintText: 'Descrição',
                         fillColor: Colors.white,
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8)
                         )
+                      ),
+                      onChanged: (value) {
+                        description = value!.trim();
+                        setState(() {});
+                      },
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              RawChip(
+                label: Text('Futebol'),
+                onSelected: (value) => {
+                  print('Cliquei no chip'),
+                  offeredSports.add(value),
+                },
+                onDeleted: () => {
+                  offeredSports.clear(),
+                },
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Possui lancheria?',
+                    style: TextTheme.of(ctx).bodyMedium?.copyWith(
+                      color: AppColors.primaryText,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                  Switch(
+                    value: hasSnackbar,
+                    onChanged: (value) => {
+                      setState(() {
+                        hasSnackbar = value;
+                      })
+                    },
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 16.0),
-            RawChip(
-              label: Text('Futebol'),
-              onSelected: (value) => {
-                print('Cliquei no chip'),
-                offeredSports.add(value),
-              },
-              onDeleted: () => {
-                offeredSports.clear(),
-              },
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Possui lancheria?',
-                  style: TextTheme.of(ctx).bodyMedium?.copyWith(
-                    color: AppColors.primaryText,
-                    fontWeight: FontWeight.w600
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Possui vestiário?',
+                    style: TextTheme.of(ctx).bodyMedium?.copyWith(
+                        color: AppColors.primaryText,
+                        fontWeight: FontWeight.w600
+                    ),
                   ),
-                ),
-                Switch(
-                  value: hasSnackbar,
-                  onChanged: (value) => {
-                    print('alterei o valor do switch: $value')
-                  },
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Possui vestiário?',
-                  style: TextTheme.of(ctx).bodyMedium?.copyWith(
-                      color: AppColors.primaryText,
-                      fontWeight: FontWeight.w600
+                  Switch(
+                    value: hasLockerRoom,
+                    onChanged: (value) => {
+                      setState(() {
+                        hasLockerRoom = value;
+                      }),
+                    },
                   ),
-                ),
-                Switch(
-                  value: hasLockerRoom,
-                  onChanged: (value) => {
-                    print('alterei o valor do switch: $value')
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            Container(
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Container(
                 height: 48,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(4),
                   color: AppColors.primary,
                 ),
                 child: TextButton(
-                  onPressed: () => {
-                    print('Cliquei no botão de cadastrar')
-                  },
+                  onPressed: () => moveToHome(ctx),
                   style: ButtonStyle(
                     backgroundColor: WidgetStateProperty.all(Colors.deepPurple),
                     minimumSize: WidgetStateProperty.all(Size(double.infinity, 48)),
@@ -178,9 +236,10 @@ class RegisterEstablishmentPage extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                )
-            ),
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
