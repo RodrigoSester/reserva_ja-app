8729a779e838c84b1e0d33c1c4c3bf817640e777
wigo <rodrigows09@gmail.com>
2025-12-19 00:27:55 -0300

    feat: add routes

---
+++ lib/utils/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static String welcomeRoute = "/welcome";
+  static String userRegisterRoute = "/user-login";
+  static String userHomeRoute = "/home/user";
+  static String establishmentRegisterRoute = "/establishment-register";
+  static String establishmentHomeRoute = "/home/establishment";
+}
